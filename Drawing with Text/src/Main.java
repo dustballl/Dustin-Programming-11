929d9c31d009c0c22b2ca49f96a77e7c025f6746
dustballl <87162161+dustballl@users.noreply.github.com>
2022-02-05 21:53:00 -0800

    Drawing with Text Assignment hand in

---
+++ Drawing with Text/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("                    _______________________________");
+        System.out.println("                   / /--------------||-----------\\ \\");
+        System.out.println("                 / /                ||            \\ \\");
+        System.out.println("   _____--------/ /_________________||_____________\\ \\");
+        System.out.println(" /|             /|                  \\                 \\");
+        System.out.println(" \\|            / |                  |                  |");
+        System.out.println("  |    _______/  |                  |       _______    |_");
+        System.out.println(" _|   /       \\  |                  |      /       \\   |_|");
+        System.out.println("|_|__|         |_|__________________|_____|         |_/_/");
+        System.out.println("      \\_______/                            \\_______/");
+    }
+}
